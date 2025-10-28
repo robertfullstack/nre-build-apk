385b52cdb73aea2c203ccb83e4e4a6112b7b9518
Robert Silva <robertcontaprofissional2023@gmail.com>
2025-10-28 17:51:43 +0000

    first commit 011

--- src/pages/Consulta.js
+++ src/pages/Consulta.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { PermissionsAndroid, Platform } from 'react-native';
+
 
 
 // import { Filesystem, Directory } from '@capacitor/filesystem';
